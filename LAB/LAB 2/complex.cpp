d15c7f52b45d4375770cf30a436595c3f900969c
kapil parajuli <parazulikapil777@gmail.com>
2022-06-08 23:48:26 +0545

    LAB 2

--- LAB/LAB 2/complex.cpp
+++ LAB/LAB 2/complex.cpp
@@ -7,30 +7,28 @@ class complex{
      double imaginary;
      public:
      void input (){
-         cout <<"enter the real part of num1 and num2 : ";
+         cout <<"enter the real part: ";
          cin>>real;
-         cout<<"enter the imaginary part of num1 and num2: ";
+         cout<<"enter the imaginary part: ";
          cin>>imaginary;
      }
-      void rsum (double  complex n1 , double complex n2){
-          double  n1 , n2;
-          double realSum= n1.real + n2.real;
-          double imaginarySum = n1.imaginary + n2.imaginary;
+      double rsum (complex n1 ,complex n2){
+          double realSum;
+           realSum= n1.real + n2.real;
+           return realSum;
+
       }
-
-
-
-
-
+      double  imgSum(complex n1, complex n2){
+          double imaginarySum;
+      imaginarySum = n1.imaginary + n2.imaginary;
+      return imaginarySum;
+      }
 
 };
 int main (){
-    complex num1,num2;
+    complex num1,num2,result;
     num1.input();
     num2.input();
-    cout<<"the sum of two complex no. is : "<sum(num1,num2)<<"+i"<sum(num1,num2)<<endl;
-
-
-
+    cout<<"the sum of two complex no. is : "<<result.rsum(num1,num2)<<"+i"<<result.imgSum(num1,num2)<<endl;
     return 0;
 }
