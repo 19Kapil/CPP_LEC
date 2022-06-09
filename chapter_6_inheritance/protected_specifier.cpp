512adfaf79497267ad0c9b2c75e4cd86420cf4be
kapil parajuli <parazulikapil777@gmail.com>
2022-06-09 08:34:39 +0545

    protected specifier

---
+++ chapter_6_inheritance/protected_specifier.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Parent{
+
+public:
+    int id_protected;
+};
+class child : public Parent
+{
+    public:
+    void setId(int id){
+    id_protected = id;
+    }
+
+void dispalyId(){
+    cout<< "id_protected is: "<<id_protected<<endl;
+
+}
+
+};
+int main(){
+    child obj1;
+    obj1.setId(81);
+    obj1.dispalyId();
+    return 0;
+}
