a6e4657b6246be9d84fed8f26abb7d93d2a10d8e
kapil parajuli <parazulikapil777@gmail.com>
2022-06-10 10:22:50 +0545

    Lab sheet 2

--- LAB/LAB 2/time.cpp
+++ LAB/LAB 2/time.cpp
@@ -1,38 +1,37 @@
-//3.	WAP to define a class Time that has 3 data members for hours, minutes and second. Include member functions 
-//void gettime(): to input time
-//void sum(Time, Time): to add 2 different time
-//void display(): to display the resultant time.
-//Time
-//hr
-//min
-//sec
-//gettime()
-//sum(Time,Time)
-//display()
+//WAP to define a class Time that has 3 data members for hours, minutes and second.
 
 #include<iostream>
 using namespace std;
-class time{
+
+class Time{
     private:
-    int hour;
-    int minute;
-    int second;
+        int hour,min,sec;
     public:
-    void gettime()
-    {
-
-
-    }
-
-
-
-
-
-
+        void gettime(){
+            cout<<"Enter time: "<<endl;
+            cout<<"\nEnter the hour: ";
+            cin>>hour;
+            cout<<"\nEnter minute: ";
+            cin>>min;
+            cout<<"\nEnter second: ";
+            cin>>sec;
+        }
+        void sum(Time one, Time two){
+            this -> sec = one.sec+two.sec;
+            this -> hour = one.hour+two.hour+(this -> min/60);
+            this -> min = one.min+two.min+(this -> sec/60);
+            this->min %=60;
+            this->sec %=60;
+        }
+        void display(){
+            cout<<"The resultant time is: "<<hour<<":"<<min<<":"<<sec<<endl;
+        }
 };
+
 int main(){
-
-
-
-    return 0;
+    Time t1,t2,add;
+    t1.gettime();
+    t2.gettime();
+    add.sum(t1,t2);
+    add.display();
 }
