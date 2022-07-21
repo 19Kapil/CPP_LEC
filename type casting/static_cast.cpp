bf7abe8d74e1c44445ccdfeec36b9cc4fb1a77c5
kapil parajuli <parazulikapil777@gmail.com>
2022-07-21 16:48:00 +0545

    static  casting.

---
+++ type casting/static_cast.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    float f = 3.124568;
+    i = f;
+    cout<<"the value of float is: "<<f<<endl;
+    cout<<"THe value of converted integer:"<<i<<endl;
+    i = static_cast<int>(f);
+    cout<<"the value of integer is : "<<i;
+}
