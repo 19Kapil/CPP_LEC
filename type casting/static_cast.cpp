b8283883bde2e3826be960d990227175bb9c9084
Kapil Parajuli <104827321+19Kapil@users.noreply.github.com>
2023-07-09 21:45:00 +0545

    Update static_cast.cpp

--- type casting/static_cast.cpp
+++ type casting/static_cast.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int i;
-    float f = 3.124568;
+    float f = 3.124568; 
     i = f;
     cout<<"the value of float is: "<<f<<endl;
     cout<<"THe value of converted integer:"<<i<<endl;
